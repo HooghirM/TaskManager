23c914bcfaa284ed626dd02c38c222473c73c075
hooghir <hooghirm29@gmail.com>
2024-05-19 17:53:16 -0400

    Navbar start

--- my-app/src/server.js
+++ my-app/src/server.js
@@ -1,4 +1,4 @@
-/*      Express server     */
+/*      Node js with Express server     */
 
 // Load express module (function)
 const express = require('express');
@@ -7,6 +7,9 @@ const express = require('express');
 'app' is used to configure the server
 , by routing requests & handling responses */
 const app = express();
+
+/* Serve static files from 'public' directory */
+app.use(express.static('public'));
 
 /* Variable 'port' connected to TCP port 3000. 
 */
@@ -17,7 +20,8 @@ const port = 3000;
 about HTTP requests/response*/
 app.get('/', (req, res) => {
     // Send 'Hello World!' as response when root URL is accessed
-  res.send('Hello World!');
+    // res.send(index.html);
+    res.send('Hello World!');
 });
 
 /* Start server so that it listens on specified port for incoming connections. 
